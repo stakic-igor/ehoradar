e6f47eab7a84d6dfe2a23c08eba98c243bd78d19
Igor Stakic <stakic_igor@yahoo.com>
2023-03-23 20:36:00 +0100

    Add target blank to social links

--- src/components/FollowUs.js
+++ src/components/FollowUs.js
@@ -4,13 +4,25 @@ const FollowUs = () => {
         <section className="follow-us">
             <span>Pratite nas na:</span>
             <div className="follow-us-boxes">
-                <a href="https://www.facebook.com/ehoradar" className="follow-us-boxes__links facebook">
+                <a 
+                    href="https://www.facebook.com/ehoradar" 
+                    className="follow-us-boxes__links facebook"
+                    target="_blank"    
+                >
                     facebook
                 </a>
-                <a href="https://www.instagram.com/ehoradar" className="follow-us-boxes__links instagram">
+                <a 
+                    href="https://www.instagram.com/ehoradar" 
+                    className="follow-us-boxes__links instagram"
+                    target="_blank"    
+                >
                     instagram
                 </a>
-                <a href="https://www.youtube.com/@ehoradar6957" className="follow-us-boxes__links youtube">
+                <a 
+                    href="https://www.youtube.com/@ehoradar6957" 
+                    className="follow-us-boxes__links youtube"
+                    target="_blank"
+                >
                     youtube
                 </a>
             </div>
