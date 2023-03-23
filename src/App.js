cf532353bc4a8a9a91f62fe2faaf77160d999e6e
Igor Stakic <stakic_igor@yahoo.com>
2023-03-23 20:33:08 +0100

    Add social links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
+import FollowUs from "./components/FollowUs";
 import Footer from "./components/Footer";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <HeroSection />
+      <FollowUs />
       <Footer />
     </>
   );
---
+++ src/components/FollowUs.js
@@ -0,0 +1,21 @@
+const FollowUs = () => {
+
+    return (
+        <section className="follow-us">
+            <span>Pratite nas na:</span>
+            <div className="follow-us-boxes">
+                <a href="https://www.facebook.com/ehoradar" className="follow-us-boxes__links facebook">
+                    facebook
+                </a>
+                <a href="https://www.instagram.com/ehoradar" className="follow-us-boxes__links instagram">
+                    instagram
+                </a>
+                <a href="https://www.youtube.com/@ehoradar6957" className="follow-us-boxes__links youtube">
+                    youtube
+                </a>
+            </div>
+        </section>
+    );
+};
+
+export default FollowUs;
