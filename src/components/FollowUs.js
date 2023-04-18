6c2ea5f7e4b295d8682ad230eee17a6743d54fc8
Igor Stakic <stakic_igor@yahoo.com>
2023-04-18 16:52:09 +0200

    Add aligment for follow us

--- src/components/FollowUs.js
+++ src/components/FollowUs.js
@@ -1,41 +1,43 @@
 const FollowUs = () => {
     return (
         <section className='follow-us'>
-            <span>Pratite nas na:</span>
-            <div className='follow-us-boxes'>
-                <a 
-                    href='https://www.facebook.com/ehoradar' 
-                    className='follow-us-boxes__links facebook'
-                    target='_blank'
-                    rel='noreferrer'
-                >
-                    <img 
-						src='/images/icons/social-icons/facebook-32.png' 
-						alt="facebook"
-					/>
-                </a>
-                <a 
-                    href='https://www.instagram.com/ehoradar' 
-                    className='follow-us-boxes__links instagram'
-                    target='_blank'
-                    rel='noreferrer'
-                >
-                    <img 
-						src='/images/icons/social-icons/instagram-32.png' 
-						alt="instagram"
-					/>
-                </a>
-                <a 
-                    href='https://www.youtube.com/@ehoradar6957' 
-                    className='follow-us-boxes__links youtube'
-                    target='_blank'
-                    rel='noreferrer'
-                >
-                    <img 
-						src='/images/icons/social-icons/youtube-32.png' 
-						alt="youtube"
-					/>
-                </a>
+            <div className="container follow-us__wrap">
+                <span>Pratite nas na:</span>
+                <div className='follow-us-boxes'>
+                    <a 
+                        href='https://www.facebook.com/ehoradar' 
+                        className='follow-us-boxes__links facebook'
+                        target='_blank'
+                        rel='noreferrer'
+                    >
+                        <img 
+                            src='/images/icons/social-icons/facebook-32.png' 
+                            alt="facebook"
+                        />
+                    </a>
+                    <a 
+                        href='https://www.instagram.com/ehoradar' 
+                        className='follow-us-boxes__links instagram'
+                        target='_blank'
+                        rel='noreferrer'
+                    >
+                        <img 
+                            src='/images/icons/social-icons/instagram-32.png' 
+                            alt="instagram"
+                        />
+                    </a>
+                    <a 
+                        href='https://www.youtube.com/@ehoradar6957' 
+                        className='follow-us-boxes__links youtube'
+                        target='_blank'
+                        rel='noreferrer'
+                    >
+                        <img 
+                            src='/images/icons/social-icons/youtube-32.png' 
+                            alt="youtube"
+                        />
+                    </a>
+                </div>
             </div>
         </section>
     );
